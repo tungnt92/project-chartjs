a55afb8fbcc07d1c69ae36227c17afbe3ba6e651
hieu <hieu.tran@gumiviet.com>
2021-04-07 18:30:26 +0700

    feat(update) : update component for Vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,120 @@
 <template>
-  <div id="my-chart">
-    {{ message }}
+  <div id="chart">
+    <!--Search-->
+    <Search/>
+
+    <!--Chart-->
+    <div class="chart__container">
+      <!--Duration-->
+      <Duration/>
+
+      <!--Project-->
+      <Project/>
+      <Project/>
+    </div>
   </div>
 </template>
 
 <script>
+  import Search from "./components/Search.vue";
+  import Duration from "./components/Duration.vue";
+  import Project from "./components/Project.vue";
+
   export default {
+    components: {
+      Project,
+      Duration,
+      Search
+    },
+
     data() {
       return {
-        message: 'Hello World',
+        chart: {
+          period_start: '03/01/2021',
+          period_end: '03/02/2021',
+          projects: [
+            {
+              id: 1,
+              name: 'Newphoria',
+              members: [
+                {
+                  id: 1,
+                  name: 'Hugh',
+                  work: [
+                    {
+                      join_date: '',
+                      leave_date: '',
+                      number: true
+                    },
+                    {
+                      join_date: '',
+                      leave_date: '',
+                      number: false
+                    }
+                  ]
+                },
+                {
+                  id: 2,
+                  name: 'Kuro',
+                  work: [
+                    {
+                      join_date: '',
+                      leave_date: '',
+                      number: false
+                    },
+                    {
+                      join_date: '',
+                      leave_date: '',
+                      number: true
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              id: 2,
+              name: 'Asura',
+              members: [
+                {
+                  id: 1,
+                  name: 'Leon',
+                  work: [
+                    {
+                      join_date: '',
+                      leave_date: '',
+                      number: false
+                    },
+                    {
+                      join_date: '',
+                      leave_date: '',
+                      number: true
+                    }
+                  ]
+                },
+                {
+                  id: 2,
+                  name: 'Chain',
+                  work: [
+                    {
+                      join_date: '',
+                      leave_date: '',
+                      number: true
+                    },
+                    {
+                      join_date: '',
+                      leave_date: '',
+                      number: false
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
       };
     },
   };
 </script>
 
 <style>
-  #my-chart {
-    font-size: 18px;
-    font-family: 'Roboto', sans-serif;
-    color: blue;
-  }
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search">
+    Search
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Search",
+
+    data () {
+      return {
+        form: {
+          period_start: '',
+          period_end: '',
+          project_name: ''
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
