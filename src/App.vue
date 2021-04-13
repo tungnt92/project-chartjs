2f25e8f4aa1095f7c3553e6679d528a988e8955f
kuro-1996 <ng.ngocanhtuan96@gmail.com>
2021-04-13 15:14:05 +0700

    fix: remove search bar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="container">
-    <ChartTable/>
+    <ChartTable />
   </div>
 </template>
 
 <script>
   import ChartTable from './components/Table.vue'
-  import Search from './components/Search.vue'
 
   export default {
     components: {
-      ChartTable,
-      Search
+      ChartTable
     }
   };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Vue from 'vue';
+import { Skeleton } from 'ant-design-vue';
 import App from './App.vue';
+import 'ant-design-vue/dist/antd.css';
+
+Vue.use(Skeleton);
 
 // eslint-disable-next-line no-undef
 window.projectChart = new Vue();
